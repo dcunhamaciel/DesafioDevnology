d8a74172de4618fbe5d1912d9d16402252d2dfa8
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-05 18:06:55 -0300

    Exibir Menu somente para usuário autenticado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,17 +27,17 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <li>
-                            <a href="{{ route('user.index') }}" class="nav-link">Usuários</a>
-                        <li>
-                    </ul>
+                @auth
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav me-auto">
+                            <li>
+                                <a href="{{ route('user.index') }}" class="nav-link">Usuários</a>
+                            <li>
+                        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        @auth
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ms-auto">
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
@@ -45,8 +45,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -54,10 +54,10 @@
                                         @csrf
                                     </form>
                                 </div>
-                            </li>
-                        @endauth
-                    </ul>
-                </div>
+                            </li>                        
+                        </ul>
+                    </div>
+                @endauth
             </div>
         </nav>
 
