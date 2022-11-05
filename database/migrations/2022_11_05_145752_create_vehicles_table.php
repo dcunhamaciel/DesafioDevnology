8bbc96c7257d6302a90daa582cefa76cdd0d8021
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-05 12:00:41 -0300

    Model de Veículos

---
+++ database/migrations/2022_11_05_145752_create_vehicles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('description', 100);
+            $table->string('brand', 100);
+            $table->string('model', 100);
+            $table->string('color', 50);
+            $table->string('manufacturing', 50);
+            $table->string('plate', 10);
+            $table->string('chassis', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
